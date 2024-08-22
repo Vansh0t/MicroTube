783346db23e65cff105a1a62a5fb0876b7f868fd
Anton Ernst <antonernst99@gmail.com>
2024-08-22 16:43:10 +0200

    moved  auth confirmation options to separate classes

---
+++ MicroTube/Services/ConfigOptions/PasswordConfirmationOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace MicroTube.Services.ConfigOptions
+{
+	public class PasswordConfirmationOptions
+	{
+		public const string KEY = "PasswordConfirmation";
+
+		public int ExpirationSeconds { get; set; }
+		public int StringLength { get; set; }
+
+		public PasswordConfirmationOptions(int expirationSeconds, int stringLength)
+		{
+			ExpirationSeconds = expirationSeconds;
+			StringLength = stringLength;
+		}
+	}
+}
