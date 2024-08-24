8966e2821c244c99f3adabde9616f606d04fcc0b
Anton Ernst <antonernst99@gmail.com>
2024-08-24 18:24:55 +0200

    added BasicFlowAuthentication unit tests

---
+++ MicroTube.Tests/Mock/MockDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MicroTube.Data.Access;
+using MicroTube.Tests.Mock.Models;
+
+namespace MicroTube.Tests.Mock
+{
+	public class MockDbContext: MicroTubeDbContext
+	{
+		public MockDbContext()
+		{
+		}
+
+		public MockDbContext(DbContextOptions<MicroTubeDbContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<MockAuthenticationData>().ToTable("MockAuthenticationData");
+		}
+	}
+}
---
+++ MicroTube.Tests/Utils/Database.cs
@@ -0,0 +1,36 @@
+﻿using EntityFramework.Exceptions.Sqlite;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using MicroTube.Data.Access;
+using MicroTube.Tests.Mock;
+
+namespace MicroTube.Tests.Utils
+{
+	internal static class Database
+	{
+		public static MicroTubeDbContext CreateSqliteInMemory()
+		{
+			var conn = new SqliteConnection("DataSource=:memory:");
+			conn.Open();
+			var options = new DbContextOptionsBuilder<MicroTubeDbContext>()
+			.UseSqlite(conn)
+			.UseExceptionProcessor()
+			.Options;
+			var context = new MicroTubeDbContext(options);
+			context.Database.EnsureCreated();
+			return context;
+		}
+		public static MockDbContext CreateSqliteInMemoryMock()
+		{
+			var conn = new SqliteConnection("DataSource=:memory:");
+			conn.Open();
+			var options = new DbContextOptionsBuilder<MicroTubeDbContext>()
+			.UseSqlite(conn)
+			.UseExceptionProcessor()
+			.Options;
+			var context = new MockDbContext(options);
+			context.Database.EnsureCreated();
+			return context;
+		}
+	}
+}
