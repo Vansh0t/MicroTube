8d04b49bbc145e05cd4960345aa93f5f6bbbcc2e
Anton Ernst <antonernst99@gmail.com>
2024-08-27 19:36:06 +0200

    moved UserSession settings into public and added dedicated Options class

---
+++ MicroTube/Services/ConfigOptions/UserSessionOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace MicroTube.Services.ConfigOptions
+{
+	public class UserSessionOptions
+	{
+		public const string KEY = "UserSession";
+
+		public int TokenLifetimeMinutes { get; set; }
+		public string RefreshPath { get; set; }
+		public UserSessionOptions(int tokenLifetimeMinutes, string refreshPath)
+		{
+			TokenLifetimeMinutes = tokenLifetimeMinutes;
+			RefreshPath = refreshPath;
+		}
+	}
+}
