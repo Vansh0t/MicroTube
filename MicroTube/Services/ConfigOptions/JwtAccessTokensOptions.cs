cf7e9962c8c481eded4c548d25f81372a9444534
Anton Ernst <antonernst99@gmail.com>
2024-08-29 14:53:58 +0200

    moved jwt tokens options into a separate class

---
+++ MicroTube/Services/ConfigOptions/JwtAccessTokensOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace MicroTube.Services.ConfigOptions
+{
+	public class JwtAccessTokensOptions
+	{
+		public const string KEY = "JwtAccessTokens";
+		public string Issuer { get; set; }
+		public string Audience { get; set; }
+		public int ExpirationMinutes { get; set; }
+		public string Key { get; set; }
+		public PasswordResetJwtAccessTokensOptions PasswordReset { get; set; }
+		public JwtAccessTokensOptions(string issuer, string audience, int expirationMinutes, string key, PasswordResetJwtAccessTokensOptions passwordReset)
+		{
+			Issuer = issuer;
+			Audience = audience;
+			ExpirationMinutes = expirationMinutes;
+			Key = key;
+			PasswordReset = passwordReset;
+		}
+	}
+}
---
+++ MicroTube/Services/ConfigOptions/PasswordResetJwtAccessTokensOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace MicroTube.Services.ConfigOptions
+{
+	public class PasswordResetJwtAccessTokensOptions
+	{
+		public int ExpirationMinutes { get; set; }
+		public PasswordResetJwtAccessTokensOptions(int expirationMinutes)
+		{
+			ExpirationMinutes = expirationMinutes;
+		}
+
+	}
+}
