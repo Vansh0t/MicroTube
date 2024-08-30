08e1c8eef4f5b301a9c10a93967e7c9c707ee76d
Anton Ernst <antonernst99@gmail.com>
2024-08-30 14:58:49 +0200

    added video file for processing integration tests

---
+++ MicroTube.Tests/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroTube.Tests
+{
+	public class Constants
+	{
+		public const string TEST_VIDEO_20S_1080P_24FPS_LOCATION = "/TestFiles/test_video_20s_1080p_24fps.mp4";
+	}
+}
