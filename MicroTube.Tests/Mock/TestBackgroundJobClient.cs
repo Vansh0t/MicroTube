53c32016c733a1b562d3735744d6fcf0fd124731
Anton Ernst <antonernst99@gmail.com>
2024-09-07 13:31:08 +0200

    mock job client to test job scheduling and argument

---
+++ MicroTube.Tests/Mock/TestBackgroundJobClient.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using Hangfire.Annotations;
+using Hangfire.Common;
+using Hangfire.States;
+
+namespace MicroTube.Tests.Mock
+{
+	public class TestBackgroundJobClient : IBackgroundJobClient
+	{
+		public Job? CreatedJob { get; private set; }
+		public bool ChangeState([NotNull] string jobId, [NotNull] IState state, [CanBeNull] string expectedState)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string Create([NotNull] Job job, [NotNull] IState state)
+		{
+			CreatedJob = job;
+			return Guid.NewGuid().ToString();
+		}
+	}
+}
