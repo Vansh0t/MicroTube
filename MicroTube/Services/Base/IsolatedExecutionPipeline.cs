7ab6ea2c283b659bd0d745f04da751ec11664be2
Anton Ernst <antonernst99@gmail.com>
2024-09-07 13:34:34 +0200

    added isolated pipeline

---
+++ MicroTube/Services/Base/IsolatedExecutionPipeline.cs
@@ -0,0 +1,37 @@
+﻿namespace MicroTube.Services.Base
+{
+	public abstract class IsolatedExecutionPipeline<TContext> : IPipeline<TContext>
+	{
+		public PipelineState State { get; protected set; }
+		protected readonly List<IPipelineStage<TContext>> stages = new();
+
+
+		public abstract Task<TContext> Execute(TContext? context, CancellationToken cancellationToken = default);
+		public void AddStage(IPipelineStage<TContext> stage)
+		{
+			ThrowIfExecuting();
+			stages.Add(stage);
+		}
+		public void InsertStage(IPipelineStage<TContext> stage, int index)
+		{
+			ThrowIfExecuting();
+			stages.Insert(index, stage);
+		}
+
+		public void RemoveStage(IPipelineStage<TContext> stage)
+		{
+			ThrowIfExecuting();
+			stages.Remove(stage);
+		}
+		public void RemoveStageAt(int index)
+		{
+			ThrowIfExecuting();
+			stages.RemoveAt(index);
+		}
+		protected void ThrowIfExecuting()
+		{
+			if (State == PipelineState.Executing)
+				throw new BackgroundJobException("Operation is not allowed while the pipeline is executing");
+		}
+	}
+}
