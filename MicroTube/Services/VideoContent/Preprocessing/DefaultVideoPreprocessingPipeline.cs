5cb5f543cde9c759dbad3be8d1ab1c8bf40d108f
Anton Ernst <antonernst99@gmail.com>
2024-09-07 13:35:54 +0200

    merged reusable code into parent class

---
+++ MicroTube/Services/VideoContent/Preprocessing/DefaultVideoPreprocessingPipeline.cs
@@ -0,0 +1,73 @@
+﻿using Azure.Storage.Blobs.Models;
+using MicroTube.Data.Access;
+using MicroTube.Data.Models;
+using MicroTube.Services.Base;
+using MicroTube.Services.MediaContentStorage;
+using MicroTube.Services.VideoContent.Preprocessing.Stages;
+
+namespace MicroTube.Services.VideoContent.Preprocessing
+{
+	public class DefaultVideoPreprocessingPipeline : IsolatedExecutionPipeline<DefaultVideoPreprocessingContext>, IVideoPreprocessingPipeline
+	{
+		private readonly ILogger<DefaultVideoPreprocessingPipeline> _logger;
+		private readonly MicroTubeDbContext _db;
+		private readonly IVideoContentRemoteStorage<AzureBlobAccessOptions, BlobUploadOptions> _remoteStorage;
+		public DefaultVideoPreprocessingPipeline(
+			IEnumerable<VideoPreprocessingStage> stages,
+			ILogger<DefaultVideoPreprocessingPipeline> logger,
+			MicroTubeDbContext db,
+			IVideoContentRemoteStorage<AzureBlobAccessOptions, BlobUploadOptions> remoteStorage)
+		{
+			foreach (var stage in stages)
+			{
+				AddStage(stage);
+			}
+			_logger = logger;
+			_db = db;
+			_remoteStorage = remoteStorage;
+		}
+		public async override Task<DefaultVideoPreprocessingContext> Execute(DefaultVideoPreprocessingContext? context, CancellationToken cancellationToken = default)
+		{
+			ThrowIfExecuting();
+			if (context == null)
+				throw new ArgumentNullException("Context must be set for this Pipeline");
+			State = PipelineState.Executing;
+			try
+			{
+				foreach (var stage in stages)
+				{
+					_logger.LogInformation("Executing video preprocessing stage: " + stage.GetType());
+					await stage.Execute(context, cancellationToken);
+				}
+				return context;
+			}
+			catch
+			{
+				try
+				{
+					State = PipelineState.Error;
+					await ClearRemoteData(context);
+					await SetUploadProgressError(context);
+				}
+				catch { }
+				throw;
+			}
+		}
+		private async Task ClearRemoteData (DefaultVideoPreprocessingContext context)
+		{
+			if (context.RemoteCache != null && !string.IsNullOrWhiteSpace(context.RemoteCache.VideoFileLocation))
+				await _remoteStorage.DeleteLocation(context.RemoteCache.VideoFileLocation); //TO DO: make into account retry possibility
+		}
+		private async Task SetUploadProgressError(DefaultVideoPreprocessingContext context)
+		{
+			if (context.UploadProgress != null)
+			{
+				_db.Update(context.UploadProgress);
+				context.UploadProgress.Status = VideoUploadStatus.Fail;
+				if (context.UploadProgress.Message == null)
+					context.UploadProgress.Message = "Preprocessing error. Please, try again later.";
+				await _db.SaveChangesAsync();
+			}
+		}
+	}
+}
--- MicroTube/Services/VideoContent/Processing/DefaultVideoProcessingPipeline.cs
+++ MicroTube/Services/VideoContent/Processing/DefaultVideoProcessingPipeline.cs
@@ -2,7 +2,6 @@
 using MicroTube.Data.Access;
 using MicroTube.Data.Models;
 using MicroTube.Services.Base;
-using MicroTube.Services.ConfigOptions;
 using MicroTube.Services.MediaContentStorage;
 using MicroTube.Services.VideoContent.Processing.Stages;
 using System.Diagnostics;
@@ -10,12 +9,8 @@ using System.IO.Abstractions;
 
 namespace MicroTube.Services.VideoContent.Processing
 {
-	public class DefaultVideoProcessingPipeline : IVideoProcessingPipeline
+	public class DefaultVideoProcessingPipeline : IsolatedExecutionPipeline<DefaultVideoProcessingContext>, IVideoProcessingPipeline
 	{
-		public PipelineState State => _state;
-
-		private PipelineState _state;
-		private readonly List<IPipelineStage<DefaultVideoProcessingContext>> _stages = new();
 		private readonly ILogger<DefaultVideoProcessingPipeline> _logger;
 		private readonly MicroTubeDbContext _db;
 		private readonly IFileSystem _fileSystem;
@@ -39,41 +34,16 @@ namespace MicroTube.Services.VideoContent.Processing
 			_fileSystem = fileSystem;
 			_remoteStorage = remoteStorage;
 		}
-
-		public void AddStage(IPipelineStage<DefaultVideoProcessingContext> stage)
-		{
-			ThrowIfExecuting();
-			_stages.Add(stage);
-		}
-
-
-		public void InsertStage(IPipelineStage<DefaultVideoProcessingContext> stage, int index)
-		{
-			ThrowIfExecuting();
-			_stages.Insert(index, stage);
-		}
-
-		public void RemoveStage(IPipelineStage<DefaultVideoProcessingContext> stage)
-		{
-			ThrowIfExecuting();
-			_stages.Remove(stage);
-		}
-
-		public void RemoveStageAt(int index)
-		{
-			ThrowIfExecuting();
-			_stages.RemoveAt(index);
-		}
-		public async Task<DefaultVideoProcessingContext> Execute(DefaultVideoProcessingContext? context, CancellationToken cancellationToken = default)
+		public async override Task<DefaultVideoProcessingContext> Execute(DefaultVideoProcessingContext? context, CancellationToken cancellationToken = default)
 		{
 			ThrowIfExecuting();
 			if (context == null)
 				throw new ArgumentNullException("InitialContext must be set for this Pipeline");
 			context.Stopwatch = Stopwatch.StartNew();
-			_state = PipelineState.Executing;
+			State = PipelineState.Executing;
 			try
 			{
-				foreach(var stage in _stages)
+				foreach(var stage in stages)
 				{
 					_logger.LogInformation("Executing video processing stage: " + stage.GetType());
 					await stage.Execute(context, cancellationToken);
@@ -85,45 +55,38 @@ namespace MicroTube.Services.VideoContent.Processing
 			{
 				try
 				{
-					_state = PipelineState.Error;
-					if (context.RemoteCache != null && !string.IsNullOrWhiteSpace(context.RemoteCache.VideoFileLocation))
-						await DeleteAllRemoteData(context.RemoteCache.VideoFileLocation);
-					if (context.UploadProgress != null)
-					{
-						_db.Update(context.UploadProgress);
-						context.UploadProgress.Status = VideoUploadStatus.Fail;
-						if (context.UploadProgress.Message == null)
-							context.UploadProgress.Message = "Unknown error";
-						await _db.SaveChangesAsync();
-					}
+					State = PipelineState.Error;
+					await HandleError(context);
 				}
 				catch { }
 				throw;
 			}
 			finally
 			{
-				_state = PipelineState.Finished;
-				if(context.LocalCache != null && context.LocalCache.WorkingLocation != null)
-				{
-					ClearLocalCache(context.LocalCache.WorkingLocation);
-				}
+				State = PipelineState.Finished;
+				ClearLocalCache(context);
 				if (context.Stopwatch != null)
 					context.Stopwatch.Stop();
 			}
 		}
-		private void ThrowIfExecuting()
+		private void ClearLocalCache(DefaultVideoProcessingContext context)
 		{
-			if (_state == PipelineState.Executing)
-				throw new BackgroundJobException("Operation is not allowed while the pipeline is executing");
+			if (context.LocalCache == null || context.LocalCache.WorkingLocation == null)
+				return;
+			_fileSystem.TryDeleteFileOrDirectory(context.LocalCache.WorkingLocation);
 		}
-		private async Task DeleteAllRemoteData(string remoteLocation)
+		private async Task HandleError(DefaultVideoProcessingContext context)
 		{
-			await _remoteStorage.DeleteLocation(remoteLocation); //TO DO: make into account retry possibility
+			if (context.RemoteCache != null && !string.IsNullOrWhiteSpace(context.RemoteCache.VideoFileLocation))
+				await _remoteStorage.DeleteLocation(context.RemoteCache.VideoFileLocation); //TO DO: make into account retry possibility
+			if (context.UploadProgress != null)
+			{
+				_db.Update(context.UploadProgress);
+				context.UploadProgress.Status = VideoUploadStatus.Fail;
+				if (context.UploadProgress.Message == null)
+					context.UploadProgress.Message = "Unknown error";
+				await _db.SaveChangesAsync();
+			}
 		}
-		private void ClearLocalCache(string workingLocation)
-		{
-			_fileSystem.TryDeleteFileOrDirectory(workingLocation);
-		}
-		
 	}
 }
