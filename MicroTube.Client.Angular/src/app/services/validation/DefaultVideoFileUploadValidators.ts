67d291b2eb744347117f4ef6ed9b96bf64686909
Anton Ernst <antonernst99@gmail.com>
2024-11-23 00:19:21 +0100

    removed misleading comment

--- MicroTube.Client.Angular/src/app/services/validation/DefaultVideoFileUploadValidators.ts
+++ MicroTube.Client.Angular/src/app/services/validation/DefaultVideoFileUploadValidators.ts
@@ -7,7 +7,7 @@ import { FileValidator } from "ngx-custom-material-file-input";
 export class DefaultVideoFileUploadValidators
 {
   private readonly ACCEPT_TYPES = "video/mp4,video/webm,video/x-msvideo,tvideo/x-ms-wmv,tvideo/quicktime";
-  readonly MAX_FILE_SIZE_BYTES = 1548576000; //100mb, cloudflare limit, need increasing later
+  readonly MAX_FILE_SIZE_BYTES = 1548576000;
   readonly MIN_TITLE_LENGTH = 2;
   readonly MAX_TITLE_LENGTH = 200;
   readonly MAX_DESCRIPTION_LENGTH = 1000;
